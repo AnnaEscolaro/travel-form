5a7abce6764b28638f3d6a6ad598a03980330609
Anna Escolaro <annaescolaro@hotmail.com>
2023-04-18 20:26:19 -0300

    ex

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const submitButton = document.getElementById('submit-btn');
 const clearButton = document.getElementById('clear-btn');
 let allInfo = document.getElementById('main-form');
-
+let checkAgreement = document.getElementById('agreement');
 function getUserValue(event) {
     event.preventDefault();
     allInfo.innerHTML = allInfo.value;
 }
 submitButton.addEventListener('click', getUserValue);
-
 function clearValues() {
     allInfo;
 }
-clearButton.addEventListener('click', clearValues);
+clearButton.addEventListener('click', clearValues());
+function changeButton() {
+    if (checkAgreement.checked) {
+        clearButton.removeAttribute('disabled');
+    }
+    if (!checkAgreement.checked) {
+        clearButton.setAttribute('button', 'disabled');
+    }
+}
+checkAgreement.addEventListener('change', changeButton());
